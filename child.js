3136c82896bd8c0a1e2f422e58158e581e107367
Planeshifter <pburckhardt@outlook.com>
2015-01-21 15:23:07 -0500

    pass index

--- child.js
+++ child.js
@@ -17,7 +17,7 @@ process.on("message", function(msg){
     case "execute":
       var ret_val = msg.datum;
       for (var i = 0; i < child.tasks.length; i++){
-        ret_val = child[child.tasks[i].namespace][child.tasks[i].function](ret_val);
+        ret_val = child[child.tasks[i].namespace][child.tasks[i].function](ret_val, msg.key);
       }
       var ret_msg = { "key" : msg.key, "value" : ret_val};
       process.send(ret_msg);
